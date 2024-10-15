5ca5ecc08096456f3daf5f5fc6b0b0b35955eb40
Epifanio Sarinana <sarinanaepifanio@gmail.com>
2024-10-14 23:48:10 -0600

    Added Log.java for logging and changed account to abstract and added accountNumber, balance and its set/get methods.

--- Account.java
+++ Account.java
@@ -1,8 +1,24 @@
-public class Account {
-    //something big
-    /*Change something
-     * or something
-     * idk
-     * ...
-     */
+public abstract class Account {
+    private int accountNumber;
+    private double balance;
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void inquiryBalance() {
+        
+    }
 }
