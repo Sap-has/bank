e0cb81608050c6a019778784338baccd32e4e2df
Epifanio Sarinana <sarinanaepifanio@gmail.com>
2024-10-15 09:27:21 -0600

    Added the methods for Customer.java

--- Customer.java
+++ Customer.java
@@ -1,3 +1,14 @@
-public class Customer {
-    
+import java.util.ArrayList;
+
+public class Customer extends Person{
+    private int customerId;
+    private ArrayList<Account> Account;
+
+    public void addAccount(Account accountToAdd) {
+
+    }
+
+    public void removeAccount( Account accountToDelete) {
+
+    }
 }
