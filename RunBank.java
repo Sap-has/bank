13a1c10efdd6e76f01f858369c7b41fd1dfa2b08
kdom55 <kayra.dominguez5@gmail.com>
2024-10-23 21:55:44 -0600

    Added code to read Bank Users.csv

--- RunBank.java
+++ RunBank.java
@@ -1,5 +1,18 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.File;
+
 public class RunBank {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException{
+        HashMap<Integer, String[]> bankUsers = new HashMap<>();
+        Scanner scnr = new Scanner(new File("info/Bank Users.csv"));
+        while(scnr.hasNextLine()){
+            String line = "";
+            String[] information = line.split(",");
+            bankUsers.put(Integer.parseInt(information[0]), information);
+        }
+        scnr.close();
+        System.out.println(bankUsers);
     }
 }
