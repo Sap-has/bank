3c90e5eaa86e9ae731b34ca05d28fe564d86ee90
kdom55 <kayra.dominguez5@gmail.com>
2024-10-23 22:08:12 -0600

    Fixed code on main to read file

--- RunBank.java
+++ RunBank.java
@@ -6,13 +6,14 @@ import java.io.File;
 public class RunBank {
     public static void main(String[] args) throws IOException{
         HashMap<Integer, String[]> bankUsers = new HashMap<>();
-        Scanner scnr = new Scanner(new File("info/Bank Users.csv"));
+        Scanner scnr = new Scanner(new File("C:\\Users\\Kayra Dominguez\\OneDrive\\Documents\\CS Codes\\CS4\\Bank\\bank\\info\\Bank Users.csv"));
+        String firstLine = scnr.nextLine();
         while(scnr.hasNextLine()){
-            String line = "";
+            String line = scnr.nextLine();
             String[] information = line.split(",");
             bankUsers.put(Integer.parseInt(information[0]), information);
         }
         scnr.close();
-        System.out.println(bankUsers);
+        System.out.println((bankUsers.get(79))[2]);
     }
 }
