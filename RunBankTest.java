0ec0082469e110f64fff1ca1c546b7a76497ccbd
kdom55 <kayra.dominguez5@gmail.com>
2024-11-03 22:09:38 -0700

    Added some junit code

--- RunBankTest.java
+++ RunBankTest.java
@@ -1,2 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class RunBankTest {
+    static RunBank bank;
+
+    @BeforeAll
+    void setupBeforeAll(){
+        bank = new RunBank();
+    }
+
+    @Test
+    void testCustomerDeposit1(){
+        bank.selectAccountAndPerformOperations();
+        assertEquals("", );
+    }
 }
