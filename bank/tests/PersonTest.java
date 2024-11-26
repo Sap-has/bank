ecedfac753440122c821559323c9bbfbb8d4f146
Epifanio Sarinana <sarinanaepifanio@gmail.com>
2024-11-25 21:56:04 -0700

    Added tests for Person class

--- bank/tests/PersonTest.java
+++ bank/tests/PersonTest.java
@@ -1,6 +1,9 @@
 package tests;
 
 import bank.Person;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -14,32 +17,32 @@ public class PersonTest {
 
     @Test
     void testGetAddress() {
-
+        assertEquals("500 W. University Ave, El Paso, TX 79968", customer.getAddress());
     }
 
     @Test
     void testGetDateOfBirth() {
-
+        assertEquals("5-Mar-39", customer.getDateOfBirth());
     }
 
     @Test
     void testGetFirstName() {
-
+        assertEquals("Daniel", customer.getFirstName());
     }
 
     @Test
     void testGetLastName() {
-
+        assertEquals("A", customer.getLastName());
     }
 
     @Test
     void testGetName() {
-
+        assertEquals("Daniel A", customer.getName());
     }
 
     @Test
     void testGetPhoneNum() {
-
+        assertEquals("(915) 747-5042", customer.getPhoneNum());
     }
 
     @AfterAll
